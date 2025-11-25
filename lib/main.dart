cc3869dbacb414da881ecc0f962a81292fa23986
firmant26 <andayanabwi@gmail.com>
2025-11-25 19:31:38 +0700

    membuat auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase_options.dart';              // gunakan config dari anggota 1
+import 'screens/login_screen_firman.dart';   // gunakan login kamu
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Inisialisasi Firebase dengan config milik anggota 1
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(MyAppGalang());
+  runApp(MyAppFirman());
 }
 
-class MyAppGalang extends StatelessWidget {
+class MyAppFirman extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Firebase Ready"),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreenFirman(),   // halaman pertama milik kamu
     );
   }
 }
